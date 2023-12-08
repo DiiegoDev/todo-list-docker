f7f78bbe350cd6cbf2a488e5965d3cdcdd1c12a2
Diego Fabbri <diegofabbri.dev@gmail.com>
2023-12-08 18:30:50 -0300

    feat: add tanstack query to call the api

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { ReactQueryProvider } from './reactQueryProvider'
 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
   title: 'Lista de afazeres',
@@ -16,7 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <ReactQueryProvider> 
+        <body className={inter.className}>
+          {children}
+        </body>
+      </ReactQueryProvider>
     </html>
   )
 }
--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,16 +1,28 @@
 'use client'
 
+import { useQuery } from '@tanstack/react-query';
 import { ChangeEvent, FormEvent, useState } from 'react';
 
 interface Todo {
   id: string;
   title: string;
   isCompleted: boolean;
-  createdAt: Date;
+  //createdAt: Date;
 }
 
 export default function Home() {
   const [title, setTitle] = useState<string | null>(null)
+
+  const fetchTodos = (): Promise<Todo[]> => 
+    fetch('http://localhost:8080/list-all').then(
+        (res) => res.json())
+  
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ['todos'],
+    queryFn: fetchTodos
+  })
+
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => setTitle(e.target.value);
 
@@ -20,22 +32,35 @@ export default function Home() {
     setTitle(null);
   }
 
+  if(isPending) {
+    return <p>loading...</p>
+  }
+
   return (
+   
     <main className="flex flex-col gap-8 items-center py-14">
       <form onSubmit={handleSubmit} className="flex gap-4">
         <input onChange={handleChange} className="bg-transparent border border-gray-200 rounded-md px-3 py-1" type="text" name="title" id="title" />
         <button type="submit" >Adicionar</button>
       </form>
 
-      <div className='min-w-[333px] flex flex-col gap-6'>
-        <div className='flex justify-between'>
-          <p>teste</p>
-          <div className='flex gap-4'>
-            <button>feito</button>
-            <button>excluir</button>
-          </div>
+      <div className='min-w-[333px]'>
+        <div className='flex flex-col gap-4'>
+         {data?.map((todo) => {
+          return (
+            <div className='flex justify-between' key={todo.id}>
+              <p>{todo.title}</p>
+
+              <div className='flex gap-4'>
+                <button className='text-green-300'>feito</button>
+                <button className='text-red-300'>excluir</button>
+              </div>
+            </div>
+          )
+         })}
         </div>
       </div>
     </main>
+   
   )
 }
